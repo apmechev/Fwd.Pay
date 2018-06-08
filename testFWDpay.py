5e229dd28f6e9934aca5f57d7edccba6fe22abb6
apmechev <apmechev@gmail.com>
2018-06-08 23:44:48 +0200

    cleanup

--- testFWDpay.py
+++ testFWDpay.py
@@ -1,7 +1,5 @@
 import FWDpay
 
-#g=FWDpay.OBPClient('bb.01.nl.nlf','FWDPay')
-#g=FWDpay.OBPClient('bb.01.nl.nl','dfasfdsFWDpay')
 g=FWDpay.OBPClient('bb.01.nl.nl','FWD.Pay')
 print(g.check_balance())
 g.send_payment(100,g.bank_id, 'FWDPay')
