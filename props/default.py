26858193988b73c7022481a0d853095e402e104c
apmechev <apmechev@gmail.com>
2018-06-08 22:28:54 +0200

    mode code movement

---
+++ props/default.py
@@ -0,0 +1,44 @@
+
+
+OUR_BANK     = 'bb.01.nl.nl'
+
+USERNAME     = 'apmechev'
+PASSWORD     = 'cRq#wqp4ByN6Z4^7'
+CONSUMER_KEY = '3npjw02pmtm5u4aw3fqojk2zijlmcy3h5tra4f45'
+
+# API server URL
+BASE_URL  = "https://beyondbanking.openbankproject.com"#/my/logins/direct" ##"https://apisandbox.openbankproject.com"
+API_VERSION  = "v2.1.0"
+
+# API server will redirect your browser to this URL, should be non-functional
+# You will paste the redirect location here when running the script
+CALLBACK_URI = 'http://127.0.0.1/cb'
+
+# Our COUNTERPARTY account id (of the same currency)
+OUR_COUNTERPARTY = '8ca8a7e4-6d02-48e3-a029-0b2bf89de9f0'
+COUNTERPARTY_BANK = 'gh.29.uk'
+# this following two fields are just used in V210
+OUR_COUNTERPARTY_ID = ''
+OUR_COUNTERPARTY_IBAN = ''
+
+
+# Our currency to use
+
+
+# Our value to transfer
+# values below 1000 do not requre challenge request
+OUR_COUNTERPARTY  = 'be4c3b50-fa7a-4e38-989b-e6d3c874a368'
+COUNTERPARTY_BANK = 'bb.03.nl.nl'
+# this following two fields are just used in V210
+OUR_COUNTERPARTY_ID = ''
+OUR_COUNTERPARTY_IBAN = ''
+
+# Our currency to use
+OUR_CURRENCY     = 'EUR'
+
+# Our value to transfer
+# values below 1000 do not requre challenge request
+OUR_VALUE = '0.01'
+OUR_VALUE_LARGE  = '1000.00'
+PAYMENT_DESCRIPTION = 'Hello Payments v2.1!'
+
