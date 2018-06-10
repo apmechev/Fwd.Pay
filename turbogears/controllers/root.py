c2e10b9336147a63c434753ba03fb6b956d40dba
apmechev <apmechev@gmail.com>
2018-06-10 07:19:27 +0200

    removing turbogears root controller default functions (They're not our development)

--- turbogears/controllers/root.py
+++ turbogears/controllers/root.py
@@ -89,10 +89,6 @@ class RootController(BaseController):
     def _before(self, *args, **kw):
         tmpl_context.project_name = "example"
 
-    @expose('example.templates.index')
-    def index(self):
-        """Handle the front-page."""
-        return dict(page='index')
 
     @expose('example.templates.FWDPay')
     def FWDpay(self, **named):
@@ -123,64 +119,10 @@ class RootController(BaseController):
         flash( '''Added Order #%s for EUR %s with runner %s'''%( count ,total, runner.name ))
         redirect( './FWDpay' )
 
-
-    @expose('example.templates.about')
-    def about(self):
-        """Handle the 'about' page."""
-        return dict(page='about')
-
     orders = OrderController()
 
     @expose('example.templates.environ')
     def environ(self):
         """This method showcases TG's access to the wsgi environment."""
         return dict(page='environ', environment=request.environ)
-
-    @expose('example.templates.data')
-    @expose('json')
-    def data(self, **kw):
-        """
-        This method showcases how you can use the same controller
-        for a data page and a display page.
-        """
-        return dict(page='data', params=kw)
-    @expose('example.templates.index')
-    @require(predicates.has_permission('manage', msg=l_('Only for managers')))
-    def manage_permission_only(self, **kw):
-        """Illustrate how a page for managers only works."""
-        return dict(page='managers stuff')
-
-    @expose('example.templates.index')
-    @require(predicates.is_user('editor', msg=l_('Only for the editor')))
-    def editor_user_only(self, **kw):
-        """Illustrate how a page exclusive for the editor works."""
-        return dict(page='editor stuff')
-
-    @expose()
-    def post_login(self, came_from=lurl('/')):
-        """
-        Redirect the user to the initially requested page on successful
-        authentication or redirect her back to the login page if login failed.
-
-        """
-        if not request.identity:
-            login_counter = request.environ.get('repoze.who.logins', 0) + 1
-            redirect('/login',
-                     params=dict(came_from=came_from, __logins=login_counter))
-        userid = request.identity['repoze.who.userid']
-        flash(_('Welcome back, %s!') % userid)
-
-        # Do not use tg.redirect with tg.url as it will add the mountpoint
-        # of the application twice.
-        return HTTPFound(location=came_from)
-
-    @expose()
-    def post_logout(self, came_from=lurl('/')):
-        """
-        Redirect the user to the initially requested page on logout and say
-        goodbye as well.
-
-        """
-        flash(_('We hope to see you soon!'))
-        return HTTPFound(location=came_from)
 
